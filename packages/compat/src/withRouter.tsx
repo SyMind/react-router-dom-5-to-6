4da2b9b1d728a8c97d2fc476760bb8a1f363c631
SyMind <dacongsama@live.com>
2023-05-14 22:56:38 +0800

    feat: match in with router

--- packages/compat/src/withRouter.tsx
+++ packages/compat/src/withRouter.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom'
+import React, { useMemo } from 'react'
+import { useLocation, useParams } from 'react-router-dom'
 import hoistStatics from 'hoist-non-react-statics'
 import type { History, Location, LocationState } from 'history'
 import { useHistory } from './useHistory'
@@ -8,15 +8,22 @@ export interface StaticContext {
   statusCode?: number | undefined;
 }
 
+export interface match<Params extends { [K in keyof Params]?: string } = {}> {
+  params: Params;
+  isExact: boolean;
+  path: string;
+  url: string;
+}
+
 export interface RouteComponentProps<
   Params extends { [K in keyof Params]?: string } = {},
   C extends StaticContext = StaticContext,
   S = LocationState,
 > {
-  history: History;
-  location: Location;
-  // match: match<Params>;
-  // staticContext?: C | undefined;
+  history: History<S>;
+  location: Location<S>;
+  match: match<Params>;
+  staticContext?: C | undefined;
 }
 
 export const withRouter = <P extends RouteComponentProps<any>, C extends React.ComponentType<P>>(Component: C & React.ComponentType<P>) => {
@@ -25,6 +32,15 @@ export const withRouter = <P extends RouteComponentProps<any>, C extends React.C
   const C: any = ({ wrappedComponentRef, ...remainingProps }: any) => {
     const location = useLocation()
     const history = useHistory()
+    const params = useParams()
+
+    const match = useMemo(() => ({
+      params,
+      // TODO
+      isExact: false,
+      path: '',
+      url: '',
+    }), [params])
 
     return (
       <Component
@@ -32,6 +48,7 @@ export const withRouter = <P extends RouteComponentProps<any>, C extends React.C
         ref={wrappedComponentRef}
         location={location}
         history={history}
+        match={match}
       />
     )
   }
